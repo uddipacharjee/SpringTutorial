65b93c8f6be8bdeffcb5cc6e03a9768b69a14b8a
uddipacharjee <bsse0730@iit.du.ac.bd>
2020-02-17 18:22:09 +0600

    Bean life cycle Test

---
+++ src/p1/Notice.java
@@ -0,0 +1,37 @@
+package p1;
+
+public class Notice {
+    private String message;
+    private int id;
+
+    public void init(){
+        System.out.println("Notice Initialzaton callback method Test");
+    }
+    public void destroy(){
+        System.out.println("Notice Destuction callback Test");
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Notice{");
+        sb.append("message='").append(message).append('\'');
+        sb.append(", id=").append(id);
+        sb.append('}');
+        return sb.toString();
+    }
+}
--- src/p1/Test.java
+++ src/p1/Test.java
@@ -1,6 +1,7 @@
 package p1;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -36,5 +37,52 @@ public class Test {
 
         Director d= (Director) factory.getBean("director");
         System.out.println(d.toString());
+
+        Director d2= (Director) factory.getBean("director2");
+        System.out.println(d.toString());
+
+        Result r= (Result) factory.getBean("result1");
+        System.out.println(r.toString());
+
+
+        // Scope prototype test
+        Notice notice= (Notice) factory.getBean("notice1");
+        notice.setId(1);
+        notice.setMessage("class will start at 3.00 pm");
+        System.out.println(notice);
+
+        Notice notice2= (Notice) factory.getBean("notice1");
+        //notice2.setId(2);
+        //notice2.setMessage("class is postponed");
+        System.out.println(notice2);
+
+        // Scope Singleton Test
+        Notice notice3= (Notice) factory.getBean("notice2");
+        notice3.setId(1);
+        notice3.setMessage("Notice A");
+        System.out.println(notice3);
+
+        Notice notice4= (Notice) factory.getBean("notice2");
+        notice4.setId(2);
+        notice4.setMessage("Notice B");
+        System.out.println(notice4);
+
+        Notice notice5= (Notice) factory.getBean("notice2");
+        //notice5.setId(3);
+        //notice5.setMessage("Notice c");
+        System.out.println(notice5);
+        // It gives the last modified object stored in singleton cache
+        System.out.println(notice3);
+
+        // Initialization callbacks
+
+        Notice notice6= (Notice) factory.getBean("notice3");
+        notice6.setId(4);
+        notice6.setMessage("Notice D");
+        System.out.println(notice6);
+        ((ConfigurableBeanFactory) factory).destroySingletons();
+
+
+
     }
 }
